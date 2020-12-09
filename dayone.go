b2dca42a325c37f559ea94c6181d6e691b98ffb2
Kim Thompson <kimthompson@hey.com>
2020-12-09 10:20:43 -0600

    this solution feels dumber, but seems like it'll run much faster

--- dayone.go
+++ dayone.go
@@ -2,7 +2,7 @@ package main
 
 import (
   "fmt"
-  "math/bits"
+  //"math/bits"
 )
 
 func main () {
@@ -209,40 +209,13 @@ func main () {
     1955,
   }
 
-  data = data[1:25]
-  
-  combos := Combinations(data, 2)
-  fmt.Println(combos)
+  dataCopy := data
+
+  for a := 0; a < len(data); a++ {
+    fmt.Printf("a at %d is %d\n", a, data[a])
+
+    for b := 0; b < len(dataCopy); b++ {
+      fmt.Printf("\tb at %d is %d\n", b, data[b])
+    }
+  }
 }
-
-// Basically copied from here but reworked to work with integers: https://github.com/mxschmitt/golang-combinations
-func Combinations(set []int, n int) (subsets [][]int) {
-	length := uint(len(set))
-
-	if n > len(set) {
-		n = len(set)
-	}
-
-	// Go through all possible combinations of objects
-	// from 1 (only first object in subset) to 2^length (all objects in subset)
-	for subsetBits := 1; subsetBits < (1 << length); subsetBits++ {
-		if n > 0 && bits.OnesCount(uint(subsetBits)) != n {
-			continue
-		}
-
-		var subset []int
-
-		for object := uint(0); object < length; object++ {
-			// checks if object is contained in subset
-			// by checking if bit 'object' is set in subsetBits
-			if (subsetBits>>object)&1 == 1 {
-				// add object to subset
-				subset = append(subset, set[object])
-			}
-		}
-		// add subset to subsets
-		subsets = append(subsets, subset)
-	}
-	return subsets
-}
-
