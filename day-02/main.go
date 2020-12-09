5688ac823ed9b9e3b7fd9d96fd79935c1c342bbc
Kim Thompson <kimthompson@hey.com>
2020-12-09 16:15:35 -0600

    solution to day 2, part 2

--- day-02/main.go
+++ day-02/main.go
@@ -50,13 +50,15 @@ func main () {
 
   fmt.Println(chalk.Magenta, "data ready!")
 
-  CountValidPasswords(cleaned)
+  //CountValidPasswordsMinCharacters(cleaned)
+  CountValidPasswordsCharacterLocation(cleaned)
 }
 
-func CountValidPasswords(passwords []Password) int {
-  fmt.Println(chalk.White, "processing . . .")
+func CountValidPasswordsMinCharacters(passwords []Password) int {
+  fmt.Println(chalk.White, "processing pt. 1 . . .")
 
   countValid := 0
+
   for _, pwd := range passwords {
     charCount := strings.Count(pwd.Password, pwd.Character)
     fmt.Println(chalk.White, "Password requires between", pwd.Min, "and", pwd.Max, "of the character", pwd.Character, "| found", charCount)
@@ -67,13 +69,48 @@ func CountValidPasswords(passwords []Password) int {
   }
 
   if countValid <= 0 {
-    log.Fatal(chalk.Red, "No valid passwords found")
+    log.Fatal(chalk.Red, "no valid passwords found")
   }
 
   fmt.Println(chalk.Green, countValid, "valid passwords found")
   return countValid
 }
 
+func CountValidPasswordsCharacterLocation(passwords []Password) int {
+  fmt.Println(chalk.White, "processing pt. 2 . . .")
+
+  countValid := 0
+
+  for _, pwd := range passwords {
+    fmt.Println(chalk.White, "Password", chalk.Magenta, pwd.Password, chalk.White, "requires", pwd.Character, "in either the", pwd.Min, "position or the", pwd.Max, "position and has a length of", len(pwd.Password))
+
+    if pwd.Min - 1 >= 0 && pwd.Max - 1 < len(pwd.Password) {
+      first := string(pwd.Password[pwd.Min - 1])
+      second := string(pwd.Password[pwd.Max - 1])
+
+      firstMatch := first == pwd.Character
+      secondMatch := second == pwd.Character
+
+      if ((firstMatch || secondMatch) && !(firstMatch && secondMatch)) {
+        fmt.Println(chalk.Blue, "is good")
+        countValid++
+      }
+      
+    } else {
+      log.Fatal(chalk.Red, "index doesn't exist")
+    }
+    
+  }
+
+  if countValid <= 0 {
+    log.Fatal(chalk.Red, "no valid passwords found")
+  }
+
+  fmt.Println(chalk.Green, countValid, "valid passwords found")
+  return countValid
+}
+
+// "Min" and "Max" don't really describe the second problem's data relationship, but it works fine. I want to use the same struct for both problems
 type Password struct {
   Min int 
   Max int
